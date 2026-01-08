2996e2d8b3f2241374924b1b09797e101d6ec191
sharif <srka780@gmail.com>
2026-01-08 10:57:27 +0600

    fix auth

--- src/auth/cookieResponse.ts
+++ src/auth/cookieResponse.ts
@@ -3,7 +3,7 @@ import { Response } from 'express';
 export default function cookieResponse(res: Response, token: string): Response {
   res.cookie('access_token', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
+    secure: true, //process.env.NODE_ENV === 'production'
     sameSite: 'none',
     // maxAge: 1 * 60 * 60 * 1000, // 1 hour
     maxAge: 1 * 60 * 60 * 1000, // 1 hour
--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -3,13 +3,12 @@ import {
   Get,
   Post,
   Body,
-  Patch,
   Param,
   Delete,
 } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
 import { CreateUrlDto } from './dto/create-dashboard.dto';
-import { UpdateDashboardDto } from './dto/update-dashboard.dto';
+// import { UpdateDashboardDto } from './dto/update-dashboard.dto';
 import { User } from 'src/auth/decorators/user.decorator';
 
 @Controller('dashboard')
@@ -34,13 +33,13 @@ export class DashboardController {
     return this.dashboardService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateDashboardDto: UpdateDashboardDto,
-  ) {
-    return this.dashboardService.update(+id, updateDashboardDto);
-  }
+  // @Patch(':id')
+  // update(
+  //   @Param('id') id: string,
+  //   @Body() updateDashboardDto: UpdateDashboardDto,
+  // ) {
+  //   return this.dashboardService.update(+id, updateDashboardDto);
+  // }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
