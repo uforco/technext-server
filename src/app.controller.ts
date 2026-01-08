b3aedfcab1f6dcd43c4f3f3a4697537c577d75a2
sharif <srka780@gmail.com>
2026-01-08 19:07:57 +0600

    visite system

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Public } from './auth/decorators/public.decorator';
 import { DashboardService } from './dashboard/dashboard.service';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
@@ -18,7 +19,12 @@ export class AppController {
 
   @Public()
   @Get(':url')
-  async visiteUrl(@Param('url') url: string): Promise<any> {
-    return await this.dashboardService.visiteUrl(url);
+  async visiteUrl(
+    @Param('url') url: string,
+    @Res() res: Response,
+  ): Promise<any> {
+    const longurl = await this.dashboardService.visiteUrl(url);
+    if (!longurl) res.redirect(`${process.env.FORTEND_URL}`);
+    else res.redirect(longurl as string);
   }
 }
